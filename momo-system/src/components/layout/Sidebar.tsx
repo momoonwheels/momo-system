8e28d8e9b3dddc521f287f5a5fd3b76b1ad9f25b
momoonwheels <momoonwheels101@gmail.com>
2026-03-28 21:52:30 -0700

    Update Sidebar.tsx

--- momo-system/src/components/layout/Sidebar.tsx
+++ momo-system/src/components/layout/Sidebar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import {
   LayoutDashboard, ClipboardList, Package, ShoppingCart,
-  Truck, Receipt, Settings, BookOpen, UtensilsCrossed, Users, TrendingUp, Banknote, FileText
+  Truck, Receipt, Settings, BookOpen, UtensilsCrossed, Users, TrendingUp, Banknote, FileText, X
 } from 'lucide-react'
 
 const nav = [
